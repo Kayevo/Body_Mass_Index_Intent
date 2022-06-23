cbbd8bb11674b4fc254f7ab2f7e26d082d7b247f
Kayevo <kayevo@protonmail.com>
2022-06-23 13:44:43 -0300

    feat (view): pass body user data between activities.

--- app/src/main/java/com/kayevo/bodymassindexintent/presentation/form/FormActivity.kt
+++ app/src/main/java/com/kayevo/bodymassindexintent/presentation/form/FormActivity.kt
@@ -1,12 +1,25 @@
 package com.kayevo.bodymassindexintent.presentation.form
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kayevo.bodymassindexintent.R
+import com.kayevo.bodymassindexintent.databinding.ActivityFormBinding
+import com.kayevo.bodymassindexintent.presentation.result.BMIActivity
 
 class FormActivity : AppCompatActivity() {
+    private lateinit var viewForm: ActivityFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        viewForm = ActivityFormBinding.inflate(layoutInflater)
+        setContentView(viewForm.root)
+
+        viewForm.generateBMI.setOnClickListener{
+            Intent(this, BMIActivity::class.java).also {
+                it.putExtra("EXTRA_WEIGHT", viewForm.weight.text.toString())
+                it.putExtra("EXTRA_HEIGHT", viewForm.height.text.toString())
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/kayevo/bodymassindexintent/presentation/result/BMIActivity.kt
@@ -0,0 +1,12 @@
+package com.kayevo.bodymassindexintent.presentation.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kayevo.bodymassindexintent.R
+
+class BMIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmiactivity)
+    }
+}
