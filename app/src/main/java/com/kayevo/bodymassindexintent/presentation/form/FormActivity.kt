1ed135ce2c442f8a335af85798b0fd38fe10cc07
Kayevo <kayevo@protonmail.com>
2022-06-22 21:15:36 -0300

    feat (view): create form activity to receive user body data.

---
+++ app/src/main/java/com/kayevo/bodymassindexintent/presentation/form/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.kayevo.bodymassindexintent.presentation.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kayevo.bodymassindexintent.R
+
+class FormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
